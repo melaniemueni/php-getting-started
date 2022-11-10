22b8ed97318dca92491964625fb0c5440d1c0c2c
melaniemueni <melaniemueni@gmail.com>
2022-11-10 13:18:03 +0300

    php-getting-started

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+echo "Hello world program";
+
+$myName = "Melanie Mueni" ;
+echo $myName;
+var_dump($myName);
+
+// interger
+$a = 30;
+$b = "30";
+$c = true;
+$d = 200.34;
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+var_dump($d);
+
+
+
+
+echo "My Name is".$myName. "My M-PESA balance is Ksh.".$d;
+?>
